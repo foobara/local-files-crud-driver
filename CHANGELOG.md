d919ffb5b58d0deeffdd651d28158b1a01586823
Miles Georgi <azimux@gmail.com>
2024-11-30 17:33:34 -0800

    Create ruby project files

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [Unreleased]
+
+- Move version.rb.erb to top-level
+
+## [0.0.1] - 2024-11-30
+
+- Initial release
---
+++ spec/foobara/local_files_crud_driver_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::LocalFilesCrudDriver do
+  it "has a version number" do
+    expect(Foobara::LocalFilesCrudDriver::VERSION).to_not be_nil
+  end
+end
